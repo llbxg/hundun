66393b20df2df2a0d5633524e15dfc40464193cb
kosh <llllbxg@gmail.com>
2021-12-30 22:28:02 +0900

    ✨ Add False Nearest Neighbors - Algorithm

--- hundun/__version__.py
+++ hundun/__version__.py
@@ -1,5 +1,5 @@
 MAJOR = 0
 MINOR = 0
-MICRO = 12
+MICRO = 13
 
 __version__ = f'{MAJOR}.{MINOR}.{MICRO}'
--- hundun/exploration/_utils.py
+++ hundun/exploration/_utils.py
@@ -3,3 +3,9 @@ import numpy as _np
 def embedding_seq(u_seq, T, D):
     idx = _np.arange(0,D,1)*T
     return _np.array([u_seq[idx+i,:] for i in range(len(u_seq)-(D-1)*T)])
+
+
+def embedding_seq_1dim(u_seq, T, D):
+    idx = _np.arange(0,D,1)*T
+    e_seq = _np.array([u_seq[idx+i,:] for i in range(len(u_seq)-(D-1)*T)])
+    return e_seq.reshape(len(e_seq), D)
