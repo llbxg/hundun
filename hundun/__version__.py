5edfbc54cf162dfdcb0076ee4d08baf5caa623ad
kosh <llllbxg@gmail.com>
2022-01-15 19:16:20 +0900

    🎨 Add wayland

--- hundun/__version__.py
+++ hundun/__version__.py
@@ -1,5 +1,5 @@
 MAJOR = 0
 MINOR = 0
-MICRO = 27
+MICRO = 28
 
 __version__ = f'{MAJOR}.{MINOR}.{MICRO}'
--- hundun/exploration/__init__.py
+++ hundun/exploration/__init__.py
@@ -4,3 +4,4 @@ from ._afn import afn
 from ._fnn import fnn
 from ._gp import calc_correlation_dimention_w_gp
 from ._utils import embedding
+from ._wayland import wayland
