aca6ff24a2a4b09b0f3f230cbe57c014488c5c22
kosh <llllbxg@gmail.com>
2022-01-16 15:07:22 +0900

    🔥 Remove `TimeSeries`

--- hundun/__version__.py
+++ hundun/__version__.py
@@ -1,5 +1,5 @@
 MAJOR = 0
 MINOR = 0
-MICRO = 28
+MICRO = 29
 
 __version__ = f'{MAJOR}.{MINOR}.{MICRO}'
--- hundun/exploration/__init__.py
+++ hundun/exploration/__init__.py
@@ -1,5 +1,3 @@
-from ..exploration._ts import TimeSeries
-
 from ._afn import afn
 from ._fnn import fnn
 from ._gp import calc_correlation_dimention_w_gp
