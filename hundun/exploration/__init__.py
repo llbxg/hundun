d6ed4750bedcb58eed7dc56d979a620a2899ffc5
kosh <llllbxg@gmail.com>
2022-01-16 17:49:15 +0900

    👻 Forgot to add

--- hundun/exploration/__init__.py
+++ hundun/exploration/__init__.py
@@ -3,5 +3,5 @@ from ._autocorrelation import autocorrelation_function
 from ._fnn import fnn
 from ._gp import calc_correlation_dimention_w_gp
 from ._mutualinfo import mutual_info
-from ._utils import embedding
+from ._utils import embedding, get_bottom
 from ._wayland import wayland
