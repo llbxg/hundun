2158ced1f08a0c9badf4bff8952cdb4cd6a59be8
kosh <llllbxg@gmail.com>
2022-01-16 15:55:22 +0900

    🎨 Add autocorrelation_function

--- hundun/__version__.py
+++ hundun/__version__.py
@@ -1,5 +1,5 @@
 MAJOR = 0
 MINOR = 0
-MICRO = 30
+MICRO = 31
 
 __version__ = f'{MAJOR}.{MINOR}.{MICRO}'
--- hundun/exploration/__init__.py
+++ hundun/exploration/__init__.py
@@ -1,4 +1,5 @@
 from ._afn import afn
+from ._autocorrelation import autocorrelation_function
 from ._fnn import fnn
 from ._gp import calc_correlation_dimention_w_gp
 from ._utils import embedding
