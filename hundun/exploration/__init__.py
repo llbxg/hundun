c9f35a0d9eebe22a960fe4f5a10d2c90bfedf32b
kosh <llllbxg@gmail.com>
2022-02-24 14:12:44 +0900

    👻 Forgot to add

--- hundun/exploration/__init__.py
+++ hundun/exploration/__init__.py
@@ -6,4 +6,5 @@ from ._mutualinfo import mutual_info
 from ._recurrenceplot import calc_recurrence_plot, show_recurrence_plot
 from ._utils import embedding, get_bottom, bartlett, get_minidx_below_seq
 from ._wayland import wayland
-from ._box3 import calc_dimension_capacity, calc_dimension_information
+from ._box3 import (calc_dimension_capacity, calc_dimension_information,
+                    calc_dimension_correlation)
