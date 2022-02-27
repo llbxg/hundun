1f355e81e53f660a31d07b4ef7ee0584fdffd22e
kosh <llllbxg@gmail.com>
2022-02-27 18:55:08 +0900

    🎨 Renew calculating for afn #8

--- hundun/__version__.py
+++ hundun/__version__.py
@@ -1,5 +1,5 @@
 MAJOR = 0
 MINOR = 1
-MICRO = 28
+MICRO = 29
 
 __version__ = f'{MAJOR}.{MINOR}.{MICRO}'
--- hundun/exploration/__init__.py
+++ hundun/exploration/__init__.py
@@ -1,6 +1,6 @@
-from ._afn import afn
+from ._afn import afn, est_dimension_w_afn
 from ._autocorrelation import acf
-from ._fnn import fnn
+from ._fnn import fnn, est_dimension_w_fnn
 from ._gp import calc_correlation_dimention_w_gp
 from ._mutualinfo import mutual_info
 from ._recurrenceplot import calc_recurrence_plot, show_recurrence_plot
