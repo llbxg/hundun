21c127ec1ba1c8d974cf82bf1a6a72650ae4281e
kosh <llllbxg@gmail.com>
2022-02-28 16:24:22 +0900

    🎨 Add simple `simple_threshold`

--- hundun/__version__.py
+++ hundun/__version__.py
@@ -1,5 +1,5 @@
 MAJOR = 0
 MINOR = 1
-MICRO = 33
+MICRO = 34
 
 __version__ = f'{MAJOR}.{MINOR}.{MICRO}'
--- hundun/exploration/__init__.py
+++ hundun/exploration/__init__.py
@@ -3,7 +3,8 @@ from ._autocorrelation import acf, est_lag_w_acf
 from ._fnn import fnn, est_dimension_w_fnn
 from ._gp import calc_correlation_dimention_w_gp
 from ._mutualinfo import mutual_info, est_lag_w_mi
-from ._recurrenceplot import calc_recurrence_plot, show_recurrence_plot
+from ._recurrenceplot import (calc_recurrence_plot, show_recurrence_plot,
+                              simple_threshold)
 from ._utils import embedding, get_bottom, bartlett, get_minidx_below_seq
 from ._wayland import wayland, est_dimension_w_wayland
 from ._box3 import (calc_dimension_capacity, calc_dimension_information,
