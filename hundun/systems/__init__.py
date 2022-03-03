be1ce62032db2f2be0f3ae6138752f285b0cef32
kosh <llllbxg@gmail.com>
2022-03-03 13:44:44 +0900

    🎨 Add `DynamicalSystems`

--- hundun/systems/__init__.py
+++ hundun/systems/__init__.py
@@ -1,4 +1,5 @@
 from ._differential import Differential
 from ._difference import Difference
 from ._sync import Synchronization, coupling_oneway
+from ._systems import DynamicalSystems
 from ._tu import TU
